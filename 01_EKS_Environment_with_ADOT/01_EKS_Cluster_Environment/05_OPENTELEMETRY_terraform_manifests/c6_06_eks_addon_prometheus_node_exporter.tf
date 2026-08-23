bc2acbc9b8ce9e82bf5ae644578b543abadcd1fd
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2026-08-24 07:20:01 +0900

    feat(adot): deploy cert-manager, KSM, node-exporter, and RBAC

---
+++ 01_EKS_Environment_with_ADOT/01_EKS_Cluster_Environment/05_OPENTELEMETRY_terraform_manifests/c6_06_eks_addon_prometheus_node_exporter.tf
@@ -0,0 +1,33 @@
+# Datasource: To get default EKS addon version compatible with EKS cluster version
+data "aws_eks_addon_version" "prometheus_node_exporter_default" {
+  addon_name = "prometheus-node-exporter"
+  kubernetes_version = data.terraform_remote_state.eks.outputs.eks_cluster_version
+}
+
+# Datasource: To get latest EKS addon version compatible with EKS cluster version
+data "aws_eks_addon_version" "prometheus_node_exporter_latest" {
+  addon_name = "prometheus-node-exporter"
+  kubernetes_version = data.terraform_remote_state.eks.outputs.eks_cluster_version
+  most_recent = true
+}
+
+# EKS Add-on: Prometheus Node Exporter
+resource "aws_eks_addon" "prometheus_node_exporter" {
+  cluster_name = data.terraform_remote_state.eks.outputs.eks_cluster_id
+  addon_name = "prometheus_node_exporter"
+  addon_version = data.aws_eks_addon_version.prometheus_node_exporter_latest.version
+  #conflict resolution
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  tags = var.tags
+}
+
+output "prometheus_node_exporter_addon_id" {
+  description = "Prometheus Node Exporter EKS Addon ID"
+  value       = aws_eks_addon.prometheus_node_exporter.id
+}
+
+output "prometheus_node_exporter_addon_version" {
+  description = "Prometheus Node Exporter EKS Addon Version"
+  value       = aws_eks_addon.prometheus_node_exporter.addon_version
+}
